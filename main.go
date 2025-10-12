4cd465b94b4737dd6ef62e276670e7d17b7fd230
Oleksandr Shestopalov <shestopalovaleks@gmail.com>
2025-10-11 20:49:00 -0700

    port 80

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 func main() {
 	app := fiber.New()
 
+
+	// When someone accesses http://<host>:1024/, this function runs.
 	app.Get("/", func(c *fiber.Ctx) error {
 		response := map[string]interface{}{
 			"message":   "My name is Olexander Shestopalov",
 			"timestamp": time.Now().Unix(),
 		}
-		return c.JSON(response)
+		return c.JSON(response)// Return the response as a JSON object to the client.
 	})
 
 	app.Listen(":80")
